eb4ad9f6541a65d9f45d4d6e4682ebfea08f00a1
cam <36940948+camwolff02@users.noreply.github.com>
2026-03-18 21:55:31 -0700

    updated dotfiles for brew

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,11 @@ set -gx NOREPLY 36940948+camwolff02@users.noreply.github.com
 
 switch (uname)
     case Linux
-        fish_add_path /home/cam/.pixi/bin
+        # fish_add_path /home/cam/.pixi/bin
     case Darwin
         alias tailscale "/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 end
+
+# Homebrew
+set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
+set -gx HOMEBREW_BUNDLE_FILE "$XDG_CONFIG_HOME/brewfile/Brewfile"
