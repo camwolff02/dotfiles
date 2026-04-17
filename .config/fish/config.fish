596e5a8a75803b706c65e4d9af16235bd5ee0eb4
Cameron Wolff <36940948+camwolff02@users.noreply.github.com>
2026-04-16 22:49:02 -0700

    fixed fish config variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,16 +6,11 @@ function fish_greeting
     fastfetch
 end
 
-alias vim nvim
-set -gx NOREPLY 36940948+camwolff02@users.noreply.github.com
-
 switch (uname)
     case Linux
-        # fish_add_path /home/cam/.pixi/bin
+        set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
     case Darwin
-        alias tailscale "/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+        # ...
 end
 
-# Homebrew
-set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
-set -gx HOMEBREW_BUNDLE_FILE "$XDG_CONFIG_HOME/brewfile/Brewfile"
+alias vim nvim
